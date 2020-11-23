a6b4ac5244e992c7d2c1736b64435e430ecd63a5
iQueFrio <73860789+iQueFrio@users.noreply.github.com>
2020-11-22 21:18:52 -0600

    Ya se automatizo todo, faltan pocos detalles con respecto a los tipos
    Falta ver lo de los tipos de estrabismo porque tuve dudas ahi xd

--- Assets/Project/Scripts/TargetMove.cs
+++ Assets/Project/Scripts/TargetMove.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class TargetMove : MonoBehaviour
 {
     EyeSettings eyesSet;
-    Vector3 eyesCopy;
-    int p;
+    GameObject pared;
+    int primtime = 0;//Esta variable es el tiempo para que se almacene bien la rotacion
+    int sectime = 0;//Esta variable es cuando una vez pasado el tiempo ya puede moverse el Tarjet
 
     [SerializeField]
     float eulerAngX;
@@ -21,59 +22,65 @@ public class TargetMove : MonoBehaviour
     void Start()
     {
         eyesSet = GameObject.Find("Eyes").GetComponent<EyeSettings>();
-        int count = eyesSet.transform.childCount;
+        pared = GameObject.Find("Occluder");
         Debug.Log(eyesSet.transform.GetChild(1).name + eyesSet.transform.GetChild(1).position.ToString("f3"));
         Debug.Log(eyesSet.transform.GetChild(0).name + eyesSet.transform.GetChild(0).position.ToString("f3"));
-        eyesCopy = eyesSet.transform.GetChild(1).localEulerAngles;
-        Debug.Log("Rotacion actual: " + eyesSet.transform.GetChild(1).localEulerAngles + "Copia: " + eyesCopy);
-        
+        Debug.Log("Posicion occluder: " + pared.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("Rotacion actual: " + eyesSet.transform.GetChild(1).localEulerAngles + "Copia: " + eyesCopy);
         eulerAngX = eyesSet.transform.GetChild(1).localEulerAngles.x;
         eulerAngY = eyesSet.transform.GetChild(1).localEulerAngles.y;
         eulerAngZ = eyesSet.transform.GetChild(1).localEulerAngles.z;
-        //valorinicial = eyesSet.transform.GetChild(1).localEulerAngles.y;
-        //Debug.Log(valorinicial);
-        if (eulerAngY < 30f)
+        if (eulerAngY + 2f < valorinicial && primtime >= 161)
         {
             Debug.Log("Menor que 30");
+            sectime = 1;
         }
-        else if (p < 100)
+        else if (primtime < 160)
         {
             valorinicial = eulerAngY;
-            //Debug.Log("Wenas :v " + valorinicial);
-            p++;
+            Debug.Log("Wenas :v " + valorinicial);
+            primtime++;
+        }
+        else if (primtime == 160)
+        {
+            pared.transform.position = new Vector3(1.42f, 0.816f, 1.24f);
+            primtime++;
         }
         Movement();
     }
 
     void Movement()
     {
-        //Los GetKey solo estan de prueba para hacerlo manual
-        if (Input.GetKey(KeyCode.D))
+
+        if (sectime == 1 && eulerAngY < valorinicial)
         {
             //El tarjet se movera hasta que la rotacion del ojo vuelva a la original
-            transform.Translate(Vector3.left * 10f * Time.deltaTime);
+            transform.Translate(Vector3.left * Time.deltaTime);
         }
+        else if (sectime == 1 && eulerAngY > valorinicial + 2.5f)
+            transform.Translate(Vector3.left * Time.deltaTime);
         /*
+        //Si el angulo es menor que el inicial, se movera a la derecha de nuestra vista
+        else if (eulerAngY < valorinicial)
+            transform.Translate(Vector3.left * 10f * Time.deltaTime);
+        //Si el angulo es mayor que el inicial, se movera a la izquierda de nuestra vista
         else if (eulerAngY > valorinicial)
             transform.Translate(Vector3.right * 10f * Time.deltaTime);
-        else if (eulerAngY < valorinicial) 
-            transform.Translate(Vector3.left * Time.deltaTime);
         */
     }
 
-    static float WrapAngle(float angle)
+    void CalcularAngulo()
     {
-        angle %= 360;
-        if (angle > 180)
-            return angle - 360;
-
-        return angle;
+        float dt = 24.2f * transform.position.x;
+        if (dt < 0)
+            dt *= -1f;
+        float Td = 400f;
+        float PD = 24.2f * 2.60f;
+        float angulo = Mathf.Atan((2f * dt - PD) / 2f * Td) + Mathf.Atan(PD/(2f * Td));
     }
 
 }
