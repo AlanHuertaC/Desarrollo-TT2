f7f686f10ac3ce4ec5055249199b79e5484bd2bb
AlanHuertaC <54279303+AlanHuertaC@users.noreply.github.com>
2020-11-29 00:45:31 -0600

    Add MySQL
    Agregue la libreria que jala con unity y hacer la peticiones mas faciles. Agregue scripts para el uso de mysql. Tambien modifique algunos scripts para el llenado de la base de datos creo ya esta todo por parte de unity y MySQL :) y en la semana les muestro el cambio que hice para usar java y unity juntos.

---
+++ Assets/Project/Scripts/ConsumirAPI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+using System.Net;
+
+
+public class ConsumirAPI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetItems();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+
+    private static void GetItems()
+    {
+        var url = $"http://localhost/API";
+        var request = (HttpWebRequest)WebRequest.Create(url);
+        request.Method = "GET";
+        request.ContentType = "application/json";
+        request.Accept = "application/json";
+
+        try
+        {
+            using (WebResponse response = request.GetResponse()) 
+            {
+                using (Stream strReader = response.GetResponseStream())
+                {
+                    if (strReader == null) return;
+                    using (StreamReader objReader = new StreamReader(strReader))
+                    {
+                        string responseBody = objReader.ReadToEnd();
+                        // Do something with responseBody
+                        Debug.Log("Devuelve " + responseBody);
+                        Api api = new Api();
+                        api = JsonUtility.FromJson<Api>(responseBody);
+                        Debug.Log(api.idPaciente);
+
+
+                    }
+                }
+            }
+        }
+        catch (WebException ex)
+        {
+            // Handle error
+        }
+    }
+}
+
+public class Api
+{
+    public string id;
+    public string idPaciente;
+    public string code;
+    public string statusMessage;
+}
+
