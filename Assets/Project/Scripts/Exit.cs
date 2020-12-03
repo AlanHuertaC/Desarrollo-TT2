0f8a864016c361b776bb898bb4c5f6a5e286613f
AlanHuertaC <54279303+AlanHuertaC@users.noreply.github.com>
2020-12-03 17:54:48 -0600

    Add button Exit
    Agregue el boton salir :v, posteriormente le hare cambios visuales a la interfaz :3

---
+++ Assets/Project/Scripts/Exit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    //Exit Application
+    public void exit()
+    {
+        Application.Quit();
+        Debug.Log("Salir");
+    }
+}
